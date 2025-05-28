3724114fcbba5ad539eb25c2e4ca166a69c3026c
OHironari <hiroball0728@gmail.com>
2025-05-28 18:49:26 +0900

    no132

--- app.py
+++ app.py
@@ -10,16 +10,19 @@ load_dotenv()
 
 # 環境変数からMongoDB URIを取得
 mongo_uri = os.getenv("MONGODB_URI")
+habits = ["Test habit","Test habit 2"]
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template("index.html",title="Habit Tracker - Home")
+    return render_template("index.html",habits=habits,title="Habit Tracker - Home")
 
 @app.route("/add",methods=["GET","POST"])
 def add_habit():
+    if request.method == "POST":
+        habits.append(request.form.get("habit"))
     return render_template("add_habit.html",title="Habit Tracker - Add Habit")
 
 
